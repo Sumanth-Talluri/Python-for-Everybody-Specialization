36d04349484716690e48a67513543567af434398
SumanthTalluri <tallurisaisumanth77@gmail.com>
2020-08-12 21:08:59 +0530

    Update Assignment 15.1.py

--- 4. Using Databases with Python/1. Our First Database/Assignment 15.1.py
+++ 4. Using Databases with Python/1. Our First Database/Assignment 15.1.py
@@ -1,9 +1,7 @@
 import sqlite3
 
-# Create a database in RAM
 db = sqlite3.connect(':memory:')
 
-# Get a cursor object
 cursor = db.cursor()
 cursor.execute('''
     CREATE TABLE Ages ( 
@@ -23,12 +21,9 @@ cursor.execute('''INSERT INTO Ages (name, age) VALUES ('Neshawn', 17)''')
 # Select user
 cursor.execute('''SELECT hex(name || age) AS X FROM Ages ORDER BY X''')
 
-# retrieve the first row
+
 user1 = cursor.fetchone()
-# Print the first column retrieved(user's name)
 print(f"The first row in the resulting record set : {user1[0]}")
 
-# Commit changes into database
 db.commit()
-# Close database
 db.close()
