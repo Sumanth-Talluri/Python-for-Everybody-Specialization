c8ab03ed814409a42366b970289ca5cb5383c145
SumanthTalluri <tallurisaisumanth77@gmail.com>
2020-08-13 11:08:41 +0530

    Add files via upload

---
+++ 5. Capstone: Retrieving, Processing, and Visualizing Data with Python/2. Loading and Modeling Mail Data/gline.js
@@ -0,0 +1,3 @@
+gline = [ ['Month','umich.edu','unl.edu','mac.com','columbia.edu','virginia.edu','unicon.net','hull.ac.uk','berkeley.edu','cam.ac.uk','mtu.edu'],
+['2005-12',25,12,10,7,6,6,6,6,5,5]
+];
